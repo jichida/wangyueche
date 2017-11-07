52438f738d204eb754633b50ed58edc2a8d1b014
wangxiaoqing <xiaoqingwang@126.com>
2017-11-07 23:56:35 +0800

    pinche driver

--- pinchedriver/src/env/config.prod.js
+++ pinchedriver/src/env/config.prod.js
@@ -2,7 +2,7 @@
  * Created by wangxiaoqing on 2017/3/27.
  */
 let config = {
-    serverurl:'http://zn.com28.cn:3004',
+    serverurl:'http://wangyueche.com28.cn',
     requesttimeout:5000,
     appversion:'1.0.0',
     sendlocationinterval:5000,
